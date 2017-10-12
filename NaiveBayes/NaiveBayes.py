28f3b99920fb40c8c99e1417ce80b5d567859a43
alchemist009 <alchemist009@gmail.com>
2017-10-11 21:58:05 -0500

    Started working on Naive Bayes classifier
    * Modified folder structure
    * Added code to process text in each email using glob
    * Filtered out stop words using included list
    * Created bloblist of all emails
    * Calculated tf-idf features for all emails
    * Added all words to dictonary (might not use this method)

---
+++ NaiveBayes/NaiveBayes.py
@@ -0,0 +1,62 @@
+import codecs
+from collections import Counter
+import os
+import glob
+from textblob import TextBlob as tb
+import math
+
+def tf(word, blob):
+	return (float)(blob.words.count(word)) / (float)(len(blob.words))
+
+def n_containing(word, bloblist):
+	return sum(1 for blob in bloblist if word in blob.words)
+
+def idf(word, bloblist):
+	return (float)(math.log(len(bloblist)) / (float)(1 + n_containing(word, bloblist)))
+
+def tfidf(word, blob, bloblist):
+	return tf(word, blob) * idf(word, bloblist)
+
+words_list = []
+stop = []
+bloblist = []
+uniquewords = []
+words_dict = {}
+
+stop_words = open("Stop_words.txt").read()
+for line in stop_words:
+	for word in line.split(' '):
+		stop.append(word)
+
+stop_string = ''.join(stop)
+
+path = "hw2_train/train/spam/*.txt"
+for file in glob.glob(path):
+	file_words = codecs.open(file, "r", encoding='utf-8', errors='ignore' )
+	for line in file_words:
+		for word in line.split():
+			if not word in stop_string:
+				words_list.append(word)
+	blob = tb(' '.join(words_list))
+	bloblist.append(blob)
+	words_list = []
+
+print(bloblist[3])
+#blob = tb(' '.join(words))
+
+# for i in words:
+# 	if not i in words_dict.keys():
+# 		count = blob.words.count(i)
+# 		words_dict[i] = count
+
+for i, blob in enumerate(bloblist):
+	print("Top words in document {}".format(i+1))
+	scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+	sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+	for word, score in sorted_words[:3]:
+		print("\tWord: {}, TF-IDF: {}".format(word, round(score, 3)))
+
+
+# for i in words_dict.keys():
+# # 	if words_dict[i] > 15:
+# 	print(i, words_dict[i])
